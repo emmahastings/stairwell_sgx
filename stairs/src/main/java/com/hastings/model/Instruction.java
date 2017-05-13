ace950450ff4340faa200c57633a94a0dbbf444d
emmahastings <emmakhastings@gmail.com>
2017-05-13 13:57:52 +0100

    Update model class to include details of required input

--- stairs/src/main/java/com/hastings/model/Instruction.java
+++ stairs/src/main/java/com/hastings/model/Instruction.java
@@ -11,11 +11,25 @@ public class Instruction {
 
     private String instructions;
 
-    public Instruction(String instructions) {
+    private String input1;
+
+    private String input2;
+
+    public Instruction(String instructions, String input1, String input2) {
         this.instructions = instructions;
+        this.input1 = input1;
+        this.input2 = input2;
     }
 
     public String getInstructions() {
         return instructions;
+    }
+
+    public String getInput1() {
+        return input1;
+    }
+
+    public String getInput2() {
+        return input2;
     }
 }
