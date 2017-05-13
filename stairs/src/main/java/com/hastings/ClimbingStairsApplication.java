d3794f96da58e7cfe087b40b4a03d99edb88e7c6
emmahastings <emmakhastings@gmail.com>
2017-05-13 13:16:58 +0100

    Create application class as entry point to program

---
+++ stairs/src/main/java/com/hastings/ClimbingStairsApplication.java
@@ -0,0 +1,19 @@
+package com.hastings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by emmakhastings on 13/05/2017.
+ *
+ * @author emmakhastings
+ *         <p>
+ *         Standard spring boot application class that acts as entry point
+ */
+@SpringBootApplication
+public class ClimbingStairsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbingStairsApplication.class, args);
+    }
+}
