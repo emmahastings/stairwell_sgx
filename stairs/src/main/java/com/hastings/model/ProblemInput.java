5b01427c26176a5b85b55323010f730164b43181
emmahastings <emmakhastings@gmail.com>
2017-05-14 09:19:50 +0100

    Add validation annotation to catch main problem specifications for inputs

--- stairs/src/main/java/com/hastings/model/ProblemInput.java
+++ stairs/src/main/java/com/hastings/model/ProblemInput.java
@@ -1,16 +1,26 @@
 package com.hastings.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by emmakhastings on 13/05/2017.
  *
  * @author emmakhastings
  *         <p>
- *         Model class to represent JSON input
+ *         Model class to represent JSON input.
+ *         Includes validation rules based on problem description
  */
 public class ProblemInput {
 
+    @NotNull(message = "Number of steps per flight cannot be null")
+    @Size(message = "A stairwell must have between 1 - 30 flights", min = 1, max = 30)
     private int[] numberOfStepsPerFlight;
 
+    @Min(value = 1, message = "Steps per stride must be between 1 - 4")
+    @Max(value = 4, message = "Steps per stride must be between 1 - 4")
     private int stepsPerStride;
 
     public ProblemInput() {
