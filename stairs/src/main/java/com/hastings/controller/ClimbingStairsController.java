974b498e1f01fc64864f14adde48f1ce8bd2cae6
emmahastings <emmakhastings@gmail.com>
2017-05-14 09:20:49 +0100

    Add exception handling for user input issues

--- stairs/src/main/java/com/hastings/controller/ClimbingStairsController.java
+++ stairs/src/main/java/com/hastings/controller/ClimbingStairsController.java
@@ -1,5 +1,7 @@
 package com.hastings.controller;
 
+import com.hastings.exception.InvalidStepsPerStride;
+import com.hastings.model.ExceptionDetails;
 import com.hastings.model.Instruction;
 import com.hastings.model.ProblemInput;
 import com.hastings.model.ProblemOutput;
@@ -7,7 +9,14 @@ import com.hastings.service.InputProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by emmakhastings on 13/05/2017.
@@ -36,8 +45,17 @@ public class ClimbingStairsController {
     }
 
     @PostMapping(produces = "application/json")
-    ResponseEntity<ProblemOutput> calculateMinNumberOfStrides(@RequestBody ProblemInput input) {
+    ResponseEntity<ProblemOutput> calculateMinNumberOfStrides(@RequestBody @Valid ProblemInput input) throws InvalidStepsPerStride {
         int minNumberOfStrides = inputProcessingService.calculateMinNumberOfStrides(input.getNumberOfStepsPerFlight(), input.getStepsPerStride());
         return new ResponseEntity<ProblemOutput>(new ProblemOutput(minNumberOfStrides), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody ExceptionDetails
+    handleBadRequest(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        List<String> errorMessage = fieldErrors.stream().map(fieldError -> fieldError.getDefaultMessage()).collect(Collectors.toList());
+        return new ExceptionDetails(HttpStatus.BAD_REQUEST.getReasonPhrase(), errorMessage.toString());
     }
 }
